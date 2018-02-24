ae3ccf4be5802aa7c00ba85002a4d8d3b21458b9
madycakes <viclee02@gmail.com>
2018-02-23 17:52:35 -1000

    add new missions

---
+++ js/app.js
@@ -0,0 +1,64 @@
+//Your final mission!
+
+//Mission 1: 
+//Team Leader: 
+
+/*Change the contents in the paragraph element with the id of `one` to the following:
+
+`Five Dollar Fridays! Donuts!`*/
+
+
+// Mission 2: 
+//Team Leader:
+
+/*Create a paragraph element with the id of `two` and give it the following content:
+
+`Making Money Moves!`
+
+Append this paragraph element into the div element with thd id of `brand`*/
+
+
+// Mission 3: 
+//Team Leader:
+
+/*Create 4 li elements with the class name of `guide`. Here are the information for each li element:
+
+`Home`
+`About Us`
+`Services`
+`Contact Us`
+
+Append these li elements into the nav element.*/
+
+// Mission 4: 
+//Team Leader:
+
+/*Target the span element with the id of `good` and change the font color to cornflowerblue via the DOM.*/
+
+// Mission 5: 
+//Team Leader: 
+
+/*Make li elements with for each element in the array below. Give the li elements a class name of `rank` and append these li elements into the ul element.*/
+
+var chicken = ['orange chicken', 'lemon chicken', 'fried chicken', 'garlic chicken', 'tandoori chicken', 'chicken nuggets'];
+
+
+// Mission 6: 
+// Team Leader:
+
+// Remove the li element that has the `snake` content
+
+
+// Mission 7: 
+//Team Leader: 
+
+/*Create a function that will generate a random number between 0 and 20. Place this number in the div element with the `random` id.*/
+
+
+// Mission 8: 
+//Team Leader:
+/*Create a function that will get the right day of the week displayed in the div element with the id of `today`*/
+
+var dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday'];
+
+
